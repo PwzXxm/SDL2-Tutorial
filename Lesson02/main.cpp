5354fa87a39e65c16f74a69a903e257d0dd923b8
PwzXxm <cepheidfov@gmail.com>
2018-06-18 18:23:20 +1000

    update indentation

--- Lesson02/main.cpp
+++ Lesson02/main.cpp
@@ -9,70 +9,71 @@ SDL_Surface *defaultSurface = NULL;
 SDL_Surface *imgSurf = NULL;
 
 bool init() {
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    std::cerr << "Failed to init SDL: " << SDL_GetError() << std::endl;
-    return false;
-  }
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Failed to init SDL: " << SDL_GetError() << std::endl;
+        return false;
+    }
 
-  window = SDL_CreateWindow("SDL Lesson 02", SDL_WINDOWPOS_UNDEFINED,
-                             SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                             SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("SDL Lesson 02", SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-  if (window == NULL) {
-    std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
-    return false;
-  }
+    if (window == NULL) {
+        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+        return false;
+    }
 
-  defaultSurface = SDL_GetWindowSurface(window);
+    defaultSurface = SDL_GetWindowSurface(window);
 
-  return true;
+    return true;
 }
 
 bool loadMedia() {
-  imgSurf = SDL_LoadBMP("./star.bmp");
-  if (imgSurf == NULL) {
-    std::cerr << "Unable to load BMP image: " << SDL_GetError() << std::endl;
-    return false;
-  }
-  return true;
+    imgSurf = SDL_LoadBMP("./star.bmp");
+    if (imgSurf == NULL) {
+        std::cerr << "Unable to load BMP image: " << SDL_GetError()
+                  << std::endl;
+        return false;
+    }
+    return true;
 }
 
 void close() {
-  SDL_FreeSurface(imgSurf);
-  imgSurf = NULL;
-  SDL_DestroyWindow(window);
-  window = NULL;
-  SDL_Quit();
+    SDL_FreeSurface(imgSurf);
+    imgSurf = NULL;
+    SDL_DestroyWindow(window);
+    window = NULL;
+    SDL_Quit();
 }
 
 int main(int argc, char *args[]) {
-  if (!init()) {
-    std::cerr << "Initialization failed!" << std::endl;
-    return -1;
-  } else {
-    // tod
-  }
-  if (!loadMedia()) {
-    std::cerr << "Load media failed" << std::endl;
-    return -1;
-  }
-  SDL_FillRect(defaultSurface, NULL,
-               SDL_MapRGB(defaultSurface->format, 255, 0, 0));
-
-  SDL_BlitSurface(imgSurf, NULL, defaultSurface, NULL);
-  SDL_UpdateWindowSurface(window);
-
-  bool loop = true;
-  while (loop) {
-    SDL_Event e;
-    while (SDL_PollEvent(&e)) {
-      if (e.type == SDL_QUIT) {
-        loop = false;
-      }
+    if (!init()) {
+        std::cerr << "Initialization failed!" << std::endl;
+        return -1;
+    } else {
+        // tod
     }
-  }
+    if (!loadMedia()) {
+        std::cerr << "Load media failed" << std::endl;
+        return -1;
+    }
+    SDL_FillRect(defaultSurface, NULL,
+                 SDL_MapRGB(defaultSurface->format, 255, 0, 0));
 
-  close();
+    SDL_BlitSurface(imgSurf, NULL, defaultSurface, NULL);
+    SDL_UpdateWindowSurface(window);
 
-  return 0;
+    bool loop = true;
+    while (loop) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                loop = false;
+            }
+        }
+    }
+
+    close();
+
+    return 0;
 }
